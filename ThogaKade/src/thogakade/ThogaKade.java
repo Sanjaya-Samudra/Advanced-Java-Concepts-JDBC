8d087385351522e021687bddfc13b0045e9665d0
Sanjaya-Samudra <sanjayasamudra2003@gmail.com>
2024-09-01 13:48:59 +0530

    Content Added - First Commit

--- ThogaKade/src/thogakade/ThogaKade.java
+++ ThogaKade/src/thogakade/ThogaKade.java
@@ -33,7 +33,7 @@ public class ThogaKade {
             System.out.println("Driver s/w not found");
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
-        }*/
+        }
 
         String SQL="Delete from Customer where name='Danapala'";
         try {
@@ -50,7 +50,68 @@ public class ThogaKade {
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
+        
+        // Shows Error - Select Queries Cannot Execute Using This Method. 
+        
+        String SQL="Select * From Customer";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/ThogaKade", "root", "password");
+            Statement stm= connection.createStatement();
+            int res=stm.executeUpdate(SQL);
+            if(res>0){
+                System.out.println("Update Success");
+                System.out.println("No of rows : "+res);
+            }            
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Driver s/w not found");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
 
+        // Correct way to execute "Select" Query.
+        
+        String SQL="Select * From Customer";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/ThogaKade", "root", "password");
+            Statement stm= connection.createStatement();
+            ResultSet rst = stm.executeQuery(SQL);
+            rst.next(); 
+            String id=rst.getString("id");
+            String name=rst.getString("name"); //2
+            String address=rst.getString("address");
+            double salary=rst.getDouble("salary");
+            System.out.println(id+"\t"+name+"\t"+address+"\t"+salary);
+            
+
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Driver s/w not found");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }*/
+        
+        // But above way only display us with a single record only. If we want to see all, need to iterate.
+        
+        String SQL="Select * From Customer";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/ThogaKade", "root", "password");
+            Statement stm= connection.createStatement();
+            ResultSet rst = stm.executeQuery(SQL);
+            while(rst.next()){ 
+                String id=rst.getString("id");
+                String name=rst.getString("name"); //2
+                String address=rst.getString("address");
+                double salary=rst.getDouble("salary");
+                System.out.println(id+"\t"+name+"\t"+address+"\t"+salary);
+            }
+
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Driver s/w not found");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
     
 }
