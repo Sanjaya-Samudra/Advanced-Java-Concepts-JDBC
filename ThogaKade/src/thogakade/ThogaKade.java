823b44a04451afcf028a28eddc1a2775ca243408
Sanjaya-Samudra <sanjayasamudra2003@gmail.com>
2024-09-01 13:20:30 +0530

    first commit

--- ThogaKade/src/thogakade/ThogaKade.java
+++ ThogaKade/src/thogakade/ThogaKade.java
@@ -4,7 +4,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 public class ThogaKade {
     public static void main(String[] args) {
-        String SQL="Insert into Customer values('C011','Don','Galle',45000)";
+        /*String SQL="Insert into Customer values('C011','Don','Galle',45000)";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/ThogaKade", "root", "2003");
@@ -17,8 +17,24 @@ public class ThogaKade {
             System.out.println("Driver s/w not found");
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
+        }*/
+        
+        String SQL="Update Customer set salary=salary*1.15";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/ThogaKade", "root", "2003");
+            Statement stm= connection.createStatement();
+            int res=stm.executeUpdate(SQL);
+            if(res>0){
+                System.out.println("Update Success");
+                System.out.println("No of rows : "+res);
+            }            
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Driver s/w not found");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
         }
-   
+
     }
     
 }
