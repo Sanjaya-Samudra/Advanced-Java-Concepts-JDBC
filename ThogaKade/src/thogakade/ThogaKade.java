bff8efddeabecb31fe1717e08b189d1eb6e0bfbc
Sanjaya-Samudra <sanjayasamudra2003@gmail.com>
2024-09-06 20:32:02 +0530

    second commit

--- ThogaKade/src/thogakade/ThogaKade.java
+++ ThogaKade/src/thogakade/ThogaKade.java
@@ -89,7 +89,7 @@ public class ThogaKade {
             System.out.println("Driver s/w not found");
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
-        }*/
+        }
         
         // But above way only display us with a single record only. If we want to see all, need to iterate.
         
@@ -112,6 +112,61 @@ public class ThogaKade {
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
+        
+        //Using text field values as input to the database by using inset into SQL statement.
+        
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        double salary = Double.parseDouble(txtSalary.getText());
+        String SQL = "INSERT INTO Customer VALUES('"+id+"','"+name+"','"+address+"','"+salary+"')";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/ThogaKade", "root", "2003");
+            Statement stm = connection.createStatement();
+            int res = stm.executeUpdate(SQL);
+            if(res > 0){
+                JOptionPane.showMessageDialog(this, "Added succesfully");
+            }
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(this, "Driver Not Found");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(this, ex.getMessage());
+        }
+        
+        //Like these we can see that values in the textFields are taken into variables and they are assigned to
+        //variables, to use in SQL statement.
+        
+        // Alternate method to do above scenario.
+        
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        double salary = Double.parseDouble(txtSalary.getText());
+        //String SQL = "INSERT INTO Customer VALUES('"+id+"','"+name+"','"+address+"','"+salary+"')";
+        String SQL = "INSERT INTO Customer VALUES(?,?,?,?)";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/ThogaKade", "root", "2003");
+            //Statement stm = connection.createStatement();
+            PreparedStatement stm = connection.prepareStatement(SQL);
+            stm.setObject(1, id);
+            stm.setObject(2, name);
+            stm.setObject(3, address);
+            stm.setObject(4, salary);
+            int res = stm.executeUpdate();
+            if(res > 0){
+                JOptionPane.showMessageDialog(this, "Added succesfully");
+            }
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(this, "Driver Not Found");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(this, ex.getMessage());
+        }
+        
+        //This method called Prepared Statement method, where we do not mention the content
+        //in the SQL, but we are taking a prepared statement, so we can set values to the given
+        //prepared statement as we decide, where numbering begin from 1 to expected columns.*/
     }
     
 }
